30bc927fde852094ff652b22dd6de69738005287
Joni Kamarainen <joni.kamarainen@tuni.fi>
2022-02-23 11:20:33 +0200

    added also dumping of validation data (file names and gt classes)

--- Matlab/dump_meta_clsloc_to_text.m
+++ Matlab/dump_meta_clsloc_to_text.m
@@ -5,6 +5,7 @@ devkit_conf
 addpath(fullfile(conf.devkitDir,'evaluation'));
 
 fprintf('CLASSIFICATION WITH LOCALIZATION TASK\n');
+fprintf(' => Dumping training data information...\n');
 
 meta_file = fullfile(conf.devkitDir,'data/meta_clsloc.mat');
 
@@ -28,4 +29,31 @@ end;
 
 fclose(fid);
 
-fprintf('Dumped %d lines to %s\n',tot_classes,dump_fname); 
+fprintf(' => Done - Dumped %d lines to %s\n',tot_classes,dump_fname); 
+
+fprintf(' => Dumping validation data information...\n');
+
+blacklisted_file = fullfile(conf.devkitDir,'data/ILSVRC2014_clsloc_validation_blacklist.txt');
+ground_truth_file = fullfile(conf.devkitDir,'data/ILSVRC2014_clsloc_validation_ground_truth.txt');
+
+blacklisted = load(blacklisted_file);
+ground_truth = load(ground_truth_file);
+
+dump_fname_val = 'DUMP_validation.txt';
+fid2 = fopen(dump_fname_val,'w');
+
+tot_val = 0;
+for val_num = 1:50000
+  if sum(blacklisted == val_num) == 0
+    tot_val = tot_val + 1;
+    gt = ground_truth(val_num);
+    img_file_prefix = sprintf('ILSVRC2012_val_%08d',val_num);
+    fprintf(fid2, '{%s} {%d}\n',...
+	    img_file_prefix,...
+	    gt);
+  end;
+end;
+
+fclose(fid);
+
+fprintf(' => Done - Dumped %d lines to %s\n',tot_val,dump_fname_val); 
